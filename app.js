b2df2144db1628527a6f5f863e7014573cbf665c
haluskua <haluskua.one@gmail.com>
2021-04-03 01:47:32 +1300

    added function for next image sliding with classList change

--- app.js
+++ app.js
@@ -19,4 +19,39 @@ const dots = Array.from(carouselNav.children);
 // Getting the width of our images
 
 const imgWidth = imgs[0].getBoundingClientRect().width;
-console.log(imgWidth);
+// console.log(imgWidth);
+
+// Arraomgom tje o,ages mext to one another 
+
+// function setImgPosition(img, index) {
+//     img.style.left = imgWidth * index + 'px';
+// }
+
+const setImgPosition = (img, index) => {
+    img.style.left = imgWidth * index + 'px';
+};
+
+imgs.forEach(setImgPosition);
+
+
+/* When we click on the right button, move image to the left*/
+
+nextButton.addEventListener("click", (e) => {
+    const currentImg = list.querySelector(".current--img");
+    // console.log(currentImg);
+    const nextImg = currentImg.nextElementSibling;
+    // console.log(nextImg);
+
+    const distToMove = nextImg.style.left;
+    // console.log(distToMove);
+
+    //*_*_*_*_*_*_*_*_ Move to the Next Image
+
+    list.style.transform = "translateX(-" + distToMove + ")";
+
+    // Removing the current--img class 
+
+    currentImg.classList.remove('current--img');
+    nextImg.classList.add('current--img');
+
+});
